213d08158fc47ad441d5b5f3114be82dafc36b16
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-02-24 16:27:59 +0900

    Update C07TypeChar.java

--- src/chap02/lectur/C07TypeChar.java
+++ src/chap02/lectur/C07TypeChar.java
@@ -4,13 +4,13 @@ public class C07TypeChar {
 	public static void main(String[] args) {
 		// 문자형 : char(2byte)
 		char var1 = ' ';
-//		char var2 = ''; xxx
+//		char var2 = ''; //x
 		char var3 = '\\';
 		char var4 = 'a';
 		char var5 = '가';
 		char var6 = '나';
 		
-		char var7 = '\uac00' ; //유니코드의 약자 : \u
+		char var7 = '\uac00'; //유니코드의 약자 : \uac00
 		System.out.println(var7);
 		
 		int var8 = var7;
