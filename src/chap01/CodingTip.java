a45786b12388d0a4cd493378c43b891e0ec76c0a
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-02-24 12:16:31 +0900

    first commit

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,19 @@
+package chap01;
+
+public class CodingTip {
+	// hint : ctrl + spacebar
+	// main + 힌트키
+	public static void main(String[] args) {
+		// 자동코드 포맷팅 : ctrl + shift + f
+		// 콘솔 출력 단축키 : sout + 힌트키 또는 sysout
+		System.out.println("출력할 텍스트");
+		// 실행 : ctrl + f11
+		// 주석 : ctrl + /
+
+		// 창 크기 변경 : ctrl + m
+		
+		//새 파일 : ctrl + n
+		
+		//저장 : ctrl + s
+	}
+}
---
+++ src/chap01/Hello.java
@@ -0,0 +1,19 @@
+package chap01;
+
+/*   '/*'하고 엔터치면 자동으로 생성 
+ *   작성자 : 김현수
+ *   작성일 : 2015. 12. 31
+ */
+
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		/* sout 으로 System.out.println자동 생성 키*/
+		System.out.println("Hello, welcome to the java world!!");
+		
+		
+		//주석 단축키 : ctrl + /
+		
+	}	//end of main
+}		//end of class
