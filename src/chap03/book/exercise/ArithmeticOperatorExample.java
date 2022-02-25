03bb7f6b06f139a9afb9e89b7b257f781069f616
hyunsu <violin0117@naver.com>
2022-02-25 15:59:44 +0900

    공부

--- src/chap03/book/exercise/ArithmeticOperatorExample.java
+++ src/chap03/book/exercise/ArithmeticOperatorExample.java
@@ -1,5 +1,22 @@
 package chap03.book.exercise;
 
 public class ArithmeticOperatorExample {
-
+	public static void main(String[] args) {
+		int v1 = 5;
+		int v2 = 2;
+		
+		int result1 = v1+v2;
+		System.out.println("result1 =" + result1);
+		
+		int result2 = v1-v2;
+		System.out.println(result2);
+		
+		int result3 = v1/v2;
+		System.out.println(result3);
+		
+		int result4 = v1*v2;
+		
+		int result5=v1%v2;
+		double result6 = (double)v1 / v2;
+	}
 }
