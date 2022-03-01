16e0e15c1c04785d7961b53835be77c3b5f8bec5
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-01 12:14:31 +0900

    연습

---
+++ src/chap99/codingbat/FactorialTest.java
@@ -0,0 +1,24 @@
+package chap99.codingbat;
+
+public class FactorialTest {
+	public static void main(String[] args) {
+		System.out.println(factorial01(4));
+		System.out.println(factorial02(5));
+	}
+	
+	static long factorial01(int n) {
+		long result;
+		if(n==1) {
+			result = 1;
+		}else {
+			result = n* factorial01(n-1); // 재귀호출
+		}
+		
+		return result;
+	}
+	
+	static long factorial02(int n) {
+		return (n==1) ? 1 : n*factorial02(n-1) ;
+		
+	}
+}
