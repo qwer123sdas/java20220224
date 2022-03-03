a248ef9a6f134c631c67a402aa51163d09fe0425
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-03 21:14:49 +0900

    Update C01String.java

--- src/chap04/lecture/string/C01String.java
+++ src/chap04/lecture/string/C01String.java
@@ -3,18 +3,20 @@ package chap04.lecture.string;
 public class C01String {
 	public static void main(String[] args) {
 		String str1 = "java";
-		
+
 		System.out.println(str1);
 		System.out.println(str1.equals("spring"));
 		System.out.println(str1.equals("java"));
-		
-		System.out.println(str1.substring(0));    // java
-		System.out.println(str1.substring(1));    // ava
-		
-		System.out.println(str1.substring(0,2));  // ja
+
+		System.out.println(str1.substring(0)); // java
+		System.out.println(str1.substring(1)); // ava
+
+		System.out.println(str1.substring(0, 2)); // ja
 		System.out.println(str1.substring(2, 4)); // va
 //		System.out.println(str1.substring(1, 5)); // error
-		System.out.println(str1.length());  //4
-		
+		System.out.println(str1.length()); // 4
+
 	}
+
+
 }
