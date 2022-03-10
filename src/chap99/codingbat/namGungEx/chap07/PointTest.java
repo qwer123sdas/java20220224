846271cc9061f06f20345e30d3cf20af34e589f4
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-10 22:18:45 +0900

    남궁성 ch7 상속 파트

---
+++ src/chap99/codingbat/namGungEx/chap07/PointTest.java
@@ -0,0 +1,39 @@
+package chap99.codingbat.namGungEx.chap07;
+
+public class PointTest {
+	public static void main(String[] args) {
+		Point3D p3 = new Point3D(1, 2, 3);
+		
+		System.out.println(p3.getLocation());
+	}
+}
+
+class Point{
+	int x;
+	int y;
+	
+	Point(int x, int y) {
+		// super();
+		this.x = x;
+		this.y = y;
+	}
+
+	String getLocation() {
+		return "x : " + x + "y : " + y;
+	}
+}
+
+class Point3D extends Point{
+	int z;
+
+	Point3D(int x, int y, int z) {
+		// super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	String getLocation() {
+		return "x : " + x + ", y : " + y + ", z : " + z; // 오버라이딘
+	}
+}
