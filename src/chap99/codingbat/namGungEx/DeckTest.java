fc1e9fd3f4c653111b222afb3ff165e30f08f1e5
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-13 12:19:49 +0900

    카드셔플 만들기

--- src/chap99/codingbat/namGungEx/DeckTest.java
+++ src/chap99/codingbat/namGungEx/DeckTest.java
@@ -41,7 +41,7 @@ class Deck {
 
 }
 
-public class DeskTest {
+public class DeckTest {
 	public static void main(String[] args) {
 		Deck d = new Deck(); // 카드 한 벌(Deck)을 만든다.
 		Card c = d.pick(0); // 섞기 전에 제일 위의 카드를 뽑는다.
