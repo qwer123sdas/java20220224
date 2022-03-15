48985340d66433824138da284e99fadbf741eace
hyunsu <violin0117@naver.com>
2022-03-15 11:46:08 +0900

    super()에 의한 문제 및 해결방법

--- src/chap07/lecture/p01inheritance/SubClass3.java
+++ src/chap07/lecture/p01inheritance/SubClass3.java
@@ -1,5 +1,18 @@
 package chap07.lecture.p01inheritance;
 
-public class SubClass3 {
+public class SubClass3 extends SuperClass3 {
+	
 
+	
+	public SubClass3() {
+//		super(); 
+		super("param"); // 상위 클래스의 기본 생성자가 없으면, 파라미터가 있는 생성자를 명시적으로 호출해야 한다. 
+		System.out.println("subclass 3 no - args constructor");
+	}
+	
+	public SubClass3(int i) {
+//		super();
+		super("ppp");
+		System.out.println("subclass 3 int-param constructor");
+	}
 }
--- src/chap07/lecture/p01inheritance/SuperClass3.java
+++ src/chap07/lecture/p01inheritance/SuperClass3.java
@@ -1,5 +1,13 @@
 package chap07.lecture.p01inheritance;
 
 public class SuperClass3 {
+	public SuperClass3() {
+		//		super();
+		//		System.out.println("supper class3 no-args Constructgor");
+	}
+
+	public SuperClass3(String a) {
+		System.out.println("super class3 String-param constructor");
+	}
 
 }
