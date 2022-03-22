c43075efe342f09eab91789a92ae265550f62521
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-22 20:34:53 +0900

    equals메소드-오버라이딩

---
+++ src/chap99/codingbat/namGungEx/chap09/Ex9_1.java
@@ -0,0 +1,33 @@
+package chap99.codingbat.namGungEx.chap09;
+
+public class Ex9_1 {
+	public static void main(String[] args) {
+		Value v1 = new Value(10);
+		Value v2 = new Value(10);
+		
+		if(v1.equals(v2)) {
+			System.out.println("v1 v2는 같습니다.");
+		}else {
+			System.out.println("v1 v2는 다릅니다.");
+		}
+	}
+}
+
+class Value{
+	int value;
+	
+	Value(int value){
+		this.value = value;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Value) {
+			Value val = (Value)obj;
+			if(this.value == val.value) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
