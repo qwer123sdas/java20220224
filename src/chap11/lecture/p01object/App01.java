4b0394aade28ad0a027408ae233b054385dc7ad7
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-22 09:48:36 +0900

    Object - toString

---
+++ src/chap11/lecture/p01object/App01.java
@@ -0,0 +1,15 @@
+package chap11.lecture.p01object;
+
+public class App01 {
+	public static void main(String[] args) {
+		Object o1 = new MyClass1();
+		
+		
+	}
+}
+
+
+// 모든클래스는 object의 하위 클래스이다.
+class MyClass1 extends Object{
+	 
+}
