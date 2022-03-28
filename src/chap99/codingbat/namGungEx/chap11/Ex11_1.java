5c35027211728f4ce64cd41a27ab41ace6ea77a3
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-03-28 21:44:57 +0900

    ArrayList 공부

--- src/chap99/codingbat/namGungEx/chap11/Ex11_1.java
+++ src/chap99/codingbat/namGungEx/chap11/Ex11_1.java
@@ -49,7 +49,8 @@ public class Ex11_1 {
 //		System.out.println("index = " + list1.indexOf(new Integer(1)));
 		// 이유 : "1"로 저장한 것은 String, 뒤의 1은 Integer의 1이라서 뒤의 Integer 1의 위치를 찾은 것
 		
-		
+//		list1.remove(1); 이건 Integer 1을 삭제하는 게 아니라 index 1에 있는 객체를 삭제시킴.
+		// 이러한 문제를 해결하기 위해선 new Integer()을 통해 제거해야함. 
 		list1.remove(new Integer(1));
 		print(list1, list2);
 		
@@ -59,6 +60,17 @@ public class Ex11_1 {
 		list1.remove(4);
 		print(list1, list2);
 		
+		//lsit1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
+		System.out.println("list1.retainAll(list2) : " + list1.retainAll(list2));
+		print(list1, list2);
+		
+		//list2에서 list1에 포함된 객체들을 삭제한다.
+		for(int i = list2.size() - 1; i >=0; i--) {
+			if(list1.contains(list2.get(i))) { // get(i)가 list1에 있는지 확인함 
+				list2.remove(i);     // 있으면 이를 삭제 시킴.
+			}
+		}
+		print(list1, list2);
 		
 		
 		
