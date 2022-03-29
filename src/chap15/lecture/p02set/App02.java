0595daeb51cd27dbcc79dce2383406d33152b68e
hyunsu <violin0117@naver.com>
2022-03-29 10:08:05 +0900

    Update App02.java

--- src/chap15/lecture/p02set/App02.java
+++ src/chap15/lecture/p02set/App02.java
@@ -6,22 +6,23 @@ public class App02 {
 	public static void main(String[] args) {
 		// 로또 : 1~45의 임의의 값 6개
 		// 임의의 값 6개를 중복없이 출력하는 코드 작성
-		
+
 		Set<Integer> set = new HashSet<>();
 
-		
-		for(int i = 0; i<6; i++) {
-			int num = (int)(Math.random()*45) + 1;
+		while(set.size()<6) {
+			int num = (int) (Math.random() * 45) + 1;
+
 			set.add(num);
 		}
 		
+		
+
 		int i = 1;
-		for(int item : set) {
+		for (int item : set) {
 			System.out.println("로또번호" + i + " : " + item);
 			i++;
+
 		}
-		
-		
-		
+
 	}
 }
