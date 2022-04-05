17ad42147dfe1a8a410ec8ec5ab32a31d8bd1be0
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-04-05 21:32:11 +0900

    Date클래스의 Set메소드와 필드 연습

--- src/chap99/codingbat/namGungEx/chap10/Ex10_1.java
+++ src/chap99/codingbat/namGungEx/chap10/Ex10_1.java
@@ -17,5 +17,17 @@ public class Ex10_1 {
 		System.out.println("이 달의 몇 일 : " + today.get(Calendar.DATE));
 		System.out.println("이 달의 몇 일 : " + today.get(Calendar.DAY_OF_MONTH));
 		System.out.println("이 해의 몇 일 : " + today.get(Calendar.DAY_OF_YEAR));
+		System.out.println("이 달의 몇 쨰 요일 : " + today.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+		System.out.println("오전_오후(0:오전, 1:오후) : " + today.get(Calendar.AM_PM));
+		System.out.println("시간(0~11) : " + today.get(Calendar.HOUR));
+		System.out.println("시간(0~23) : " + today.get(Calendar.HOUR_OF_DAY));
+		System.out.println("분(0~59) : " + today.get(Calendar.MINUTE));
+		System.out.println("초(0~59) : " + today.get(Calendar.SECOND));
+		System.out.println("1000분의 1초(0~999) : " + today.get(Calendar.MILLISECOND));
+		
+		System.out.println("------------------------------------------");
+		System.out.println("TimeZone(-12~12) : " + today.get(Calendar.ZONE_OFFSET));
+		System.out.println("TimeZone(-12~12) : " + today.get(Calendar.ZONE_OFFSET)/(60*60*1000));
+		System.out.println("이달의 마지막 날: " + today.getActualMaximum(Calendar.DATE));
 	}
 }
