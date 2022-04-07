1da84f48eb64b9969ca5e1aec65e2763cd9ed2c3
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-04-07 22:07:06 +0900

    형식클래스

---
+++ src/chap99/codingbat/namGungEx/chap10/Ex10_6.java
@@ -0,0 +1,25 @@
+package chap99.codingbat.namGungEx.chap10;
+
+import java.text.DecimalFormat;
+
+public class Ex10_6 {
+	public static void main(String[] args) {
+		double number = 12389.0;
+		String[] patterns= {				
+				"0",
+				"#",
+				"0.0",
+				"#.#",
+				"0000000000.0000",
+				"##########.####",
+				"0E0",
+				"#E0"	
+		};
+		
+		for(String pattern : patterns) {
+			DecimalFormat df = new DecimalFormat(pattern);
+			System.out.printf("%19s : %s\n", pattern, df.format(number));
+		}
+		
+	}
+}
