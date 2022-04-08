02f44433445689eef23e3aaf810131736862abfb
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-04-08 20:00:30 +0900

    SimpleDateFormata 연습

---
+++ src/chap99/codingbat/namGungEx/chap10/Ex10_8.java
@@ -0,0 +1,25 @@
+package chap99.codingbat.namGungEx.chap10;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Ex10_8 {
+	public static void main(String[] args) {
+		Date today = new Date();
+		
+		SimpleDateFormat sdf1, sdf2, sdf3, sdf4;
+		SimpleDateFormat sdf5, sdf6, sdf7, sdf8, sdf9;
+		
+		sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+		sdf2 = new SimpleDateFormat("''yy년 MMM dd일 E요일");
+		sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		sdf4 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss a");
+		
+		System.out.println(sdf1.format(today));
+		System.out.println(sdf2.format(today));
+		System.out.println(sdf3.format(today));
+		System.out.println(sdf4.format(today));
+		System.out.println();
+//		System.out.println(sdf1.format(today))
+	}
+}
